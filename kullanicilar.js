b01052bb0b655332161b76ceccac0ed17c5b1ad8
Delil Noyan <noyandelil1903@hotmail.com>
2017-11-29 11:06:27 +0300

    SinavUygulamasi

---
+++ kullanicilar.js
@@ -0,0 +1,28 @@
+﻿[
+    {
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",
+        
+       "soyisim": "Aygün",
+       "profil_resmi": "orhan.jpg",
+       "d_tarihi": "01.01.1985",
+       "mezun_oldgu_okul": "Gazi Üni",
+       "fakulte": "ESEF",
+       "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+       "konular":["c#","javascript","html"]
+    },
+    {
+       "kullanici_adi": "onrknr",
+       "sifre": "321987",
+       "isim": "Onur",
+
+       "soyisim": "Kenar",
+       "profil_resmi": "resim.jpg",
+       "d_tarihi": "11.09.1989",
+       "mezun_oldgu_okul": "Mehmet Akif",
+       "fakulte": "Yok",
+       "ozgecmis": "Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum",
+       "konular": ["c#", "javascript", "html","T-SQL","Asp.NET MVC"]
+    }
+]
